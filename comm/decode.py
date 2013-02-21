3286c50962068759cff9a480fb391f29d068e422
ondrej <ondrejh.ck@gmail.com>
2013-02-21 11:06:21 +0100

    improved code capture - visualisation

--- comm/decode.py
+++ comm/decode.py
@@ -6,7 +6,7 @@ portname='COM6'
 #portname='/dev/ttyACM0'
 hellostring='M430IR_V001'
 
-codestoread=1
+codestoread=5
 codescnt=0
 codes = []
 lines = []
@@ -23,15 +23,11 @@ def decode_line(line):
 			linesplit = linesplit[1].split(';')
 			if len(linesplit)!=linelen:
 				return('Number of entries doesn\'t fit') #error 3
-			valnow = 0
-			val=[]
 			tim=[]
 			for entid in range(len(linesplit)):
 				n=int(linesplit[entid],base=16)
-				valnow=valnow^0x01
-				val=val+[valnow]
 				tim=tim+[n]
-			return(linelen,val,tim) #no error (just return values)
+			return(linelen,tim) #no error (just return values)
 
 		except:
 			return('Can\'t read entries') #error 2
@@ -39,65 +35,70 @@ def decode_line(line):
 	except:
 		return('Can\'t find line length') #error 1
 
-def expand_to_plot(val,tim):
+def expand_to_plot(decoded_line):
 	''' expand value/time lists to plot it '''
 
-	if (len(val)>0) and (len(val)==len(tim)):
-		tabs=0
-		vabs=0
-		if val[0]==0:
-			vabs=1
-		tout=[-1000,0]
-		vout=[vabs,vabs]
-		for listid in range(len(val)):
-			tout+=[tabs]
-			tabs=tim[listid]
-			vabs=val[listid]
-			tout+=[tabs]
+	try:
+		if decoded_line[0]==len(decoded_line[1]):
+			tabs=0
+			vabs=0
+			tout=[-1000,0]
+			vout=[vabs,vabs]
+			for listid in range(decoded_line[0]):
+				tout+=[tabs]
+				tabs=decoded_line[1][listid]
+				tout+=[tabs]
+				vabs^=0x01
+				vout+=[vabs,vabs]
+			vabs^=0x01
+			tout+=[tabs,tabs+1000]
 			vout+=[vabs,vabs]
-		vabs^=0x01
-		tout+=[tabs,tabs+1000]
-		vout+=[vabs,vabs]
-		return(vout,tout) #return values
-	
-	else:
-		return('Lists must be the same length longer than 0') #error
+			return(vout,tout) #return values
+		else:
+			return('Lists must be the same length longer than 0') #error
+		
+	except:
+		return('Wrong decoded line format') #error
 
 
 
 if __name__ == "__main__":
-        ''' main module: open port, test if M430IR connected, read codes '''
-        
-        with Serial(portname,9600,timeout=0.5) as port:
-                port.write('?'.encode('ascii'))
-                port.flush()
-                answ = port.readlines()
-                if len(answ)>0:
-                        answ=answ[len(answ)-1].strip().decode('ascii')
-                        print('Init module answer: {}'.format(answ))
+	''' main module: open port, test if M430IR connected, read codes '''
+	
+	with Serial(portname,9600,timeout=0.5) as port:
+		port.write('?'.encode('ascii'))
+		port.flush()
+		answ = port.readlines()
+		if len(answ)>0:
+			answ=answ[len(answ)-1].strip().decode('ascii')
+			print('Init module answer: {}'.format(answ))
 
-                        while (True):
-                                line = port.readline()
-                                if len(line)>0:
-                                        line=line.strip().decode('ascii')
-                                        print(line)
-                                        lindec=decode_line(line)
-                                        #print(lindec)
-                                        if type(lindec[0])==int:
-                                                codes+=[lindec]
-                                                lines+=[expand_to_plot(lindec[1],lindec[2])]
-                                                codescnt+=1
-                                                if codescnt>=codestoread:
-                                                        break
-                                                #print(expand_to_plot(lindec[1],lindec[2]))
+			while (True):
+				line = port.readline()
+				if len(line)>0:
+					line=line.strip().decode('ascii')
+					#print(line)
+					lindec=decode_line(line)
+					print(lindec)
+					
+					if type(lindec[0])==int:
+						codes+=[lindec]
+						lines+=[expand_to_plot(lindec)]
+						#print(expand_to_plot(lindec))
+						codescnt+=1
+						if codescnt>=codestoread:
+							break
 
-                        port.close()
+			port.close()
 
-                else:
-                        print('Init module answer: No reponse .. break')
+		else:
+			print('Init module answer: No reponse .. break')
 
-        from pylab import *
+	from pylab import *
 
-        figure(1)
-        plot(lines[0][1],lines[0][0])
-        show()
+	figure(1)
+	for line in lines:
+		plot(line[1],line[0])
+	grid('on')
+	axis([None,None,-0.5,1.5])
+	show()
